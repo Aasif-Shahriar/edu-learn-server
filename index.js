d8d6829bb9a3c9028f34a7e06ea88dae0a782cc2
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-06-15 22:58:46 +0600

    created latest course and specific course route

--- index.js
+++ index.js
@@ -2,8 +2,58 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 //middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uzfctdd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const courseCollections = client.db("eduLearn").collection("courses");
+
+    //get all the courses
+    app.get("/courses", async (req, res) => {
+      const cursor = courseCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //get limited course
+    app.get("/courses/latest", async (req, res) => {
+      const cursor = courseCollections.find().sort({ _id: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //find/get a single course for details
+    app.get("/courses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await courseCollections.findOne(query);
+      res.send(result)
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Edu-Learn-Server");
